89db964fa805f15f98c3bfbb73a14c540d910b53
elzzup <guild0105@gmail.com>
2014-08-19 06:22:47 +0900

    add new tweetlog graph

--- application/views/logpage.php
+++ application/views/logpage.php
@@ -16,7 +16,7 @@ function tag_svg_log(array $tl_log) {
 					}
 					?>
 				</div>
-				<svg class="tweet-log-svg">
+				<svg class="tweet-log-svg svg-panel" style="display: none">
 				<g transform="translate(5,0)">
 				<?php
 				for ($hour = 0; $hour < 24; $hour++) {
@@ -30,11 +30,44 @@ function tag_svg_log(array $tl_log) {
 							echo '<rect y="' . $y . '" fill="hsl(100, 0%, 95%)" stroke="#fff" width="' . RECT_SIZE_W . '" height="' . RECT_SIZE_H . '" />';
 						} else {
 							// 色
-							$h = 200;
+							$h = 150 + 150 * $log->num_par_max();
 							$s = $log->num_par_max() * 50 + 50;
 							$v = 90 - (80 * $log->num_par_max());
 							echo '<rect y="' . $y . '" fill="hsl(' . $h . ', ' . $s . '%, ' . $v . '%)" stroke="#fff" width="' . RECT_SIZE_W . '" height="' . RECT_SIZE_H . '" />';
 						}
+					}
+					echo '</g>';
+				}
+				?>
+				</g>
+				</svg>
+				<svg class="tweet-log-svg svg-line" >
+				<g transform="translate(5,0)">
+				<?php
+				$ymax = RECT_SIZE_H * 12;
+				$sum_par = 12 * 0.5;
+				for ($hour = 0; $hour < 24; $hour++) {
+
+					$p = $sum_par / 12;
+					// 色
+					$h = 150 + 150 * $p;
+					$s = $p * 40 + 40;
+					$v = 90 - (50 * $p);
+					$x = $hour * (RECT_SIZE_W + 2);
+
+					echo '<rect x="' . $x . '" y="0" width="' . (RECT_SIZE_W + 2) . '" height="' . $ymax . '" style="fill:hsl(' . $h . ', ' . $s . '%, ' . $v . '%);stroke-width:0" />';
+					$sum_par = 0;
+
+					for ($m = 0; $m < 60; $m += 5) {
+						/* @var $log Tweetlogobj */
+						$log = @$day[$hour][$m];
+						if (!$log) {
+							continue;
+						}
+						$x++;
+						$y = $ymax * $log->num_par_max();
+						$sum_par += $log->num_par_max();
+						echo '<line x1="' . $x . '" y1="' . $ymax . '" x2="' . $x . '" y2="' . ($ymax - $y) . '" style="stroke:rgb(10,10,140);stroke-width:1.5" />';
 					}
 					echo '</g>';
 				}
@@ -55,6 +88,9 @@ function tag_svg_log(array $tl_log) {
 		<div class="logs-box">
 			<div class="half" id="tl-log-box">
 				<h3 class="sub-title">@arzzupのTLの盛り上がり</h3>
+				<p>Tweet数を集計してるもの</p>
+				<input class="btn btn-mini btn-slope" type="button" id="switch-tweet-log" value="表示チェンジ" />
+				
 				<div class="tweet-log-svg-box">
 					<?php tag_svg_log($tl_log) ?>
 				</div>
