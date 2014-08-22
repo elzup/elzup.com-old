82727a97db7e4f7cc19c6a6060395646665e43ea
elzzup <guild0105@gmail.com>
2014-08-22 17:29:30 +0900

    refix headers

--- application/views/head.php
+++ application/views/head.php
@@ -12,14 +12,10 @@
 <link href='<?= FONT_GOOGLE_ALDRICH ?>' rel='stylesheet' type='text/css'>
 <link rel="stylesheet" type="text/css" href="<?= URL_YAHOO_RESET_CSS ?>">
 
-<link rel="stylesheet" type="text/css" href="<?= base_url(PATH_LIB_ANIME) ?>" />
-<link rel="stylesheet" type="text/css" href="<?= base_url(PATH_LIB_MAGIC) ?>" />
 <link rel="stylesheet" type="text/css" href="<?= base_url(PATH_LIB_LIGHTBOX_CSS) ?>" />
 
 <!-- Bootstrap -->
 <link rel="stylesheet" charset="UTF-8" type="text/css" href="<?= base_url(PATH_STYLE_CSS_MAIN) ?>" media="only screen" />
-<link rel="stylesheet" charset="UTF-8" type="text/css" href="<?= base_url(PATH_STYLE_CSS_MD) ?>" media="only screen and (min-width: 481px) and (max-width: 910px)" >
-<link rel="stylesheet" charset="UTF-8" type="text/css" href="<?= base_url(PATH_STYLE_CSS_XS) ?>" media="only screen and (max-width: 480px)" />
 <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
 <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
 <!--[if lt IE 9]> <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script> <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script> <![endif]-->
