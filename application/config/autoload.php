8544fb3fa5da916824be195123d87c4c6aa9f18c
elzzup <guild0105@gmail.com>
2015-05-29 10:59:56 +0900

    create composer install

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -52,7 +52,7 @@ $autoload['packages'] = array();
 |	$autoload['libraries'] = array('database', 'session', 'xmlrpc');
 */
 
-$autoload['libraries'] = array('session', 'metaobj', 'productionobj', 'tweetlogobj', 'dsyogilogobj', 'dsyogiprofobj', 'workobj',);
+$autoload['libraries'] = array('session', 'metaobj', 'productionobj', 'logobj', 'dsyogilogobj', 'dsyogiprofobj', 'workobj',);
 
 
 /*
